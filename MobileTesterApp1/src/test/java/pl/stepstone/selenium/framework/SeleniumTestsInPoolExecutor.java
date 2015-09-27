6666d4c9727ab56609dd000341e6e3ea8d3006e5
Maciej Uberna <ubernam@gmail.com>
2015-09-27 11:34:53 +0200

    Thred pool executor clas added to handle the tests.

---
+++ MobileTesterApp1/src/test/java/pl/stepstone/selenium/framework/SeleniumTestsInPoolExecutor.java
@@ -0,0 +1,11 @@
+package pl.stepstone.selenium.framework;
+
+import com.google.gson.JsonObject;
+
+public class SeleniumTestsInPoolExecutor {
+	
+	public SeleniumTestsInPoolExecutor(String testName, JsonObject testProperties, JsonObject allDriversProperties ){
+		
+	}
+	
+}
